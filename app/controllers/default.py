9a8fa9640ca930daa3565f8e7cad9acb70f92a77
Daniel Cenoz <danielcenoz@Daniels-MacBook-Air.local>
2022-01-23 17:23:32 -0300

    404 tratado con 'errorhandler'. Agregado Bootstrap 5 a index.html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return "<h1>hello world from Python Flask!</h1></h2>New Line</h2>"
+    return render_template('index.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+#404 handler
+@app.errorhandler(404)
+def page_not_found(error):
+    return "<h1>Esta pagina no existe</h1><br><a href='/'>Volver a pagina inicial</a>"
